513ced61e87093a1d4057b74797fa6c599b5cb62
Your Name <vasipallieshan@gmail.com>
2026-04-24 09:50:58 +0800

    created login pages, but nuked the logic as i want to pursue an alternative system for login

--- index.js
+++ index.js
@@ -22,22 +22,19 @@ const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 const supabaseClient = createClient(supabaseUrl, supabaseKey);
 
-// helps get a very ncie background for the login page, from a select set that i chose :)
 function randoimg(){
     const path = 'views/images';
-    const images =['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg','12.jpg']
-    // get a random image from this array/folder
+    const images =['1.jpg','2.jpg','3.jpg','4.jpg','5.jpg','6.jpg','7.jpg','8.jpg','9.jpg','10.jpg','11.jpg']
     const img = images[Math.floor(Math.random() * images.length)]
     return img;
 }
-
 app.route('/').get((req, res) => {
-    const provider = req.query.provider || 'MasterCard';
-    const bckimg = 'assets/bcks/' + randoimg();
-    const provider_img = 'https://vectorseek.com/wp-content/uploads/2021/02/vectorseek.com-Mastercard-Logo-Vector.png';
-    res.render('login', { provider, provider_img, bckimg });
+    res.render('test');
 });
 
+app.route('/dashboard').get((req, res) => {
+    res.render('dashboard');
+});
 
 //JUST SERVER STUFF
 app.listen(3000, () => {
